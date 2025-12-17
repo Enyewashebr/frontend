e90fba040fad5a78d7cb6d2706d84eb3fe8ade82
Enyewashebr <Enyewashebir0@gmail.com>
2025-12-17 09:41:26 +0300

    updated service component

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http"
+import CryptoJs from "crypto-js";
+import {Observable} from "rxjs";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private static BASE_URL = 'http://localhost:4200/api';
+  private static ENCRPTION_KEY = "MY-encrption key"
+
+  constructor(private http:HttpClient) { }
 }
